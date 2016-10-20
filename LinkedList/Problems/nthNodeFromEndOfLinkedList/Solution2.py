fa165795319f47fc5a193c96c2e13679095ddbee
Mohammed Fahad Kaleem <mohammedfahadkaleem@gmail.com>
2016-10-20 13:40:52 -0500

    Added solutions

--- LinkedList/Problems/nthNodeFromEndOfLinkedList/Solution2.py
+++ LinkedList/Problems/nthNodeFromEndOfLinkedList/Solution2.py
@@ -1,13 +1,12 @@
 """
 Author: Mohammed Fahad Kaleem
 
+
 Problem: Find the nth node from the end of Linked List
 
 Method:
-1) Calculate the length of Linked List. Let the length be len.
-2) Print the (length – n + 1)th node from the beginning of the Linked List.
-
 """
+
 
 class Node:
     def __init__(self,data,next_node=None):
@@ -53,27 +52,3 @@ class LinkedList(object):
                 print("[%s]"%current_node.get_data(),end=" ==> ")
                 current_node = current_node.get_next_node()
             print()
-
-    def nth_node(self,n):
-        nth_node = self.length - n + 1
-        if self.length < nth_node:
-            print("Value of n is greater than the length of the linked list")
-            return False
-        else:
-            current_node = self.head
-            for i in range(nth_node - 1):
-                current_node = current_node.get_next_node()
-            print("Nth node from last is: %s "%current_node.get_data())
-            return current_node.get_data()
-
-
-if __name__ == "__main__":
-    linked_list = LinkedList()
-    linked_list.insert(12)
-    linked_list.insert(16)
-    linked_list.insert(3)
-    linked_list.insert(15)
-    linked_list.print_linked_list()
-    linked_list.nth_node(3)
-
-
