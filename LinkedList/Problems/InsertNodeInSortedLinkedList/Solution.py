44160a1c499e5e0d0e44f2f10a63419b2c771bb1
Mohammed Fahad Kaleem <mohammedfahadkaleem@gmail.com>
2016-10-20 14:29:51 -0500

    Added solutions

---
+++ LinkedList/Problems/InsertNodeInSortedLinkedList/Solution.py
@@ -0,0 +1,10 @@
+"""
+Author: Mohammed Fahad Kaleem
+
+Problem: Insert a Node in a Sorted Linked List
+
+Method:
+1. Traverse the list to find the appropriate position
+2. Insert the node in that position
+"""
+
